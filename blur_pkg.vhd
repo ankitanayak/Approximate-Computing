5891f0ac315a436d69359cf8c6581a2d989f811a
ankitanayak <nayak.ankita25@gmail.com>
2016-10-12 22:49:07 -0400

    Add files via upload

--- blur_pkg.vhd
+++ blur_pkg.vhd
@@ -7,7 +7,9 @@ package blur_pkg is
 	constant operand_width: integer:=12;
 	constant result_width: integer:=operand_width*2;
 
-	type blur_matrix_int_type is array (0 to (8*8)-1) of integer;
+	type blur_matrix_int_type is array (0 to (matrix_size*matrix_size)-1) of integer;
+	type blur_neighbor_row_type is array (0 to matrix_size+1) of integer;
+	type blur_neighbor_column_type is array (0 to matrix_size-1) of integer;
  
  	type blur_res_matrix_row_type is array (0 to matrix_size-1) of std_logic_vector(result_width-1 downto 0);
 	type blur_res_matrix_type is array (0 to matrix_size-1) of blur_res_matrix_row_type;
